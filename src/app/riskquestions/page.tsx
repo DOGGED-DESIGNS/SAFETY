6b7bd02a29d2a66a087eb33c5d005e7db7f6df8e
dogged <uzoechijerry@gmail.com>
2025-04-20 15:50:32 +0100

    more

--- src/app/riskquestions/page.tsx
+++ src/app/riskquestions/page.tsx
@@ -5,33 +5,235 @@ import { Button } from "@/components/ui/button";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function page() {
   return (
     <Maxwrapper newClass=" max-w-screen-sm my-[100px]">
-      <div className="  flex flex-col space-y-5">
-        <Link
-          href={"#"}
-          className=" items-center flex flex-row gap-3 border-2 border-safeLight rounded-xl p-2 "
-        >
-          <Risk className="  text-safeLight fill-current h-10 w-10" />
-          <h2 className=" m-0 flex-1 p font-bold  text-safeLight ">
-            {" "}
-            i want to service for an organization{" "}
-          </h2>
-          <ChevronRight className=" text-safeDark" />
-        </Link>
-        <div className=" items-center flex flex-row gap-2 border-2 border-slate-600 rounded-xl p-3 ">
-          <h2 className=" m-0 flex-1 h4 ">
-            {" "}
-            i want to service for an organization{" "}
-          </h2>
-          <ChevronRight className=" text-safeDark" />
+      <ScrollArea className="relative overflow-auto   h-80 px-3 ">
+        <div
+          aria-hidden="true"
+          className="absolute z-10 inset-x-0 bottom-0 h-20 bg-gradient-to-t from-white/90 pointer-events-none"
+        />
+        <div className="flex flex-col   space-y-5">
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center  flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
+          <Link
+            href={"#"}
+            className=" group hover:bg-safeDark  items-center flex flex-row gap-3 border-2 border-safeDark rounded-xl p-2 "
+          >
+            <Risk className=" group-hover:text-safeWhite text-safeDark fill-current h-6 w-6" />
+            <h2 className="group-hover:text-white m-0 flex-1 p font-bold  text-safeDark ">
+              {" "}
+              i want to service for an organization{" "}
+            </h2>
+            <ChevronRight className="group-hover:text-safeWhite text-safeDark" />
+          </Link>
         </div>
-      </div>
+      </ScrollArea>
       {/* this is the button section */}
       <div className=" flex border mt-[60px]">
-        <Button className=" p font-bold ml-auto h-14"> Proceed </Button>
+        <Button className=" p font-bold ml-auto h-12 bg-safeLight hover:bg-safeDark">
+          {" "}
+          Proceed
+          <ChevronRight />
+        </Button>
       </div>
     </Maxwrapper>
   );
