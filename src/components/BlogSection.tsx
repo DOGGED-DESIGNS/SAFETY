0e18c17a1e6b00fbbc8fded9658418d84d84f199
dogged <uzoechijerry@gmail.com>
2025-06-29 21:15:52 +0100

    something

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,3 +1,4 @@
+"use server"
 import React, { cache } from "react";
 import Maxwrapper from "./Maxwrapper";
 import Image from "next/image";
@@ -6,7 +7,8 @@ import { db } from "@/db/connect";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export const revalidate = 60
+export const revalidate = 60 ;
+
 
 const getblogs = cache(async () => {
   try {
